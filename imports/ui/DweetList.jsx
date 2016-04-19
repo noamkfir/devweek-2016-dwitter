e3a2c810d9eb6241ced551d9b8846cb9e14955a6
Noam Kfir <noam@kfir.cc>
2016-04-20 00:55:51 +0100

    Applied the ReactMeteorData mixin with hard-coded data.

--- imports/ui/DweetList.jsx
+++ imports/ui/DweetList.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import reactMixin from 'react-mixin';
+import { ReactMeteorData } from 'meteor/react-meteor-data';
 
 export default class DweetList extends React.Component {
+    getMeteorData() {
+        return {
+            dweets: [
+                {
+                    content: 'It\'s just a flesh wound!',
+                    created: new Date()
+                }
+            ]
+        };
+    }
+
     render() {
         return (
             <section>
                 <h2>All Dweets</h2>
                 <ul>
                     {
-                        this.props.dweets.map((dweet, index) => {
+                        this.data.dweets.map((dweet, index) => {
                             return (
                                 <li key={index}>
                                     <div className="message">{dweet.content}</div>
-                                    <time dateTime={dweet.created.toISOString()}>{dweet.created.toLocaleString('en-GB')}</time>
+                                    <time
+                                        dateTime={dweet.created.toISOString()}>{dweet.created.toLocaleString('en-GB')}</time>
                                 </li>
                             );
                         })
@@ -22,11 +36,4 @@ export default class DweetList extends React.Component {
     }
 }
 
-DweetList.defaultProps = {
-    dweets: [
-        {
-            content: 'It\'s just a bunny!',
-            created: new Date()
-        }
-    ]
-};
+reactMixin(DweetList.prototype, ReactMeteorData);
