43d560acadcfd0cef044ca49172bf8491d239ff1
Noam Kfir <noam@kfir.cc>
2016-04-19 19:24:38 +0100

    Created the first component.

--- imports/startup/render.jsx
+++ imports/startup/render.jsx
@@ -1,7 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
+import App from '../ui/App';
 
 Meteor.startup(() => {
-    render(<div>Hello, DevWeek!</div>, document.getElementById('app'));
+    render(<App />, document.getElementById('app'));
 });
