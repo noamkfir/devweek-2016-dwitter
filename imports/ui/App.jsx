7ffd0d044549b64aeea74d2d576c43b2807f579f
Noam Kfir <noam@kfir.cc>
2016-04-19 23:52:47 +0100

    Added basic layout.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <div>Hello, DevWeek!</div>
+            <div>
+                <h1>dwitter</h1>
+                <main>
+                    <section>
+                        <h2>New Dweet</h2>
+                    </section>
+                    <section>
+                        <h2>All Dweets</h2>
+                    </section>
+                </main>
+            </div>
         );
     }
 }
