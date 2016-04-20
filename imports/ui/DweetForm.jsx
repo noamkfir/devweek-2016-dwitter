228606e00727b85c432d6c3f2bd6ec5ecefffc95
Noam Kfir <noam@kfir.cc>
2016-04-20 01:28:36 +0100

    Added data entry based on component state.

--- imports/ui/DweetForm.jsx
+++ imports/ui/DweetForm.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Dweets } from '../api/model/Dweets';
 
 export default class DweetForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {content: ''};
+    }
+
+    handleContentChange(e) {
+        const content = e.target.value;
+        this.setState({content});
+    }
+
+    handleSubmit() {
+        const created = new Date();
+        const {content} = this.state;
+        const dweet = {content, created};
+        Dweets.insert(dweet);
+    }
+
     render() {
         return (
             <section>
                 <h2>New Dweet</h2>
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div className="field">
                         <label htmlFor="contentField">Say something completely different!</label>
-                        <input id="contentField" />
+                        <input
+                            id="contentField"
+                            value={this.state.content}
+                            onChange={this.handleContentChange.bind(this)}
+                        />
                     </div>
                     <div className="commands">
                         <button type="submit">Dweet it!</button>
