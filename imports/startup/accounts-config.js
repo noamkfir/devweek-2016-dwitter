802e66f8b3ee9cb2c9561014e4ee6ce37be9514c
Noam Kfir <noam@kfir.cc>
2016-04-20 02:34:13 +0100

    Added accounts with ui and basic config.

---
+++ imports/startup/accounts-config.js
@@ -0,0 +1,3 @@
+import { AltAccounts } from 'meteor/alt:react-accounts-unstyled';
+
+AltAccounts.config.passwordSignupFields = 'USERNAME_ONLY';
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import DweetContainer from './DweetContainer';
+import { LogInButtons } from 'meteor/alt:react-accounts-ui';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>dwitter</h1>
+                <LogInButtonsDialog />
                 <main>
                     <DweetContainer />
                 </main>
